7dbdda4b1af6c739944303a430857901d236dfc8
Leeran <leeran7@gmail.com>
2022-11-11 17:56:18 -0500

    fix previous button

--- components/PageButton.tsx
+++ components/PageButton.tsx
@@ -6,21 +6,18 @@ export const Button: React.FC<{
 }> = ({ label, isNext = false }) => {
   const router = useRouter();
   const { query } = router;
-  const onClick = () => {
-    const page = query.page ? parseInt(query.page as string) : 1;
-    router.push("/?page=" + (isNext ? page + 1 : page - 1));
-  };
 
-  if (query.page) {
-    if (query.page === "1" && !isNext) return null;
-    return (
-      <button
-        className="bg-gray-200 hover:bg-gray-300 py-2 px-5 rounded"
-        onClick={onClick}
-      >
-        {label}
-      </button>
-    );
-  }
-  return null;
+  if (query.page === "1" && !isNext) return null;
+  if (!query.page && !isNext) return null;
+  return (
+    <button
+      className="bg-gray-200 hover:bg-gray-300 py-2 px-5 rounded"
+      onClick={() => {
+        const page = query.page ? parseInt(query.page as string) : 1;
+        router.push("/?page=" + (isNext ? page + 1 : page - 1));
+      }}
+    >
+      {label}
+    </button>
+  );
 };
