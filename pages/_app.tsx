908b1ffc8c514ba9ab55e042384d0659100b3234
gary <gary@Garys-MacBook-Pro.local>
2022-11-13 16:04:06 -0500

    add search bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -60,6 +60,30 @@ const NavBar = () => {
             </div>
           </div>
 
+          <div className="mx-auto max-w-md">
+            <form action="" className="relative mx-auto w-max">
+              <input
+                type="search"
+                name="search"
+                className="peer cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none text-white focus:w-full focus:cursor-text focus:border-gray-300 focus:pl-16 focus:pr-4"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 peer-focus:border-gray-300 peer-focus:stroke-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </form>
+          </div>
+
           <div className="ml-10 space-x-4">
             {user ? (
               <button
