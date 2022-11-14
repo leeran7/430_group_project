8fccb1e392b0eb2ba4bf2c37dbfe6c2ff1f88295
Leeran <leeran7@gmail.com>
2022-11-13 20:55:56 -0500

    deployment ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import { useState, useEffect } from "react";
 import { RawgApiClient } from "../components/rawgApiClient";
 import { Game } from "../types";
