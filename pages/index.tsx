2dc82923eae7038bf472a61a976bcf2c155e42dc
ChengWang11 <cheng.wang@digital-tectonics.com>
2022-11-17 23:13:20 -0500

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,6 +69,14 @@ const Home: NextPage = () => {
                       Add to cart
                     </button>
                   )}
+                  {user && (
+                    <button 
+                    onClick={() => alert(game.id)}
+                    className="absolute top-0 right-0 text-red-500 background-transparent font-bold uppercase px-2 py-3 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
+                    type="button">
+                      <svg className="w-6 h-6" fill="red" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                    </button>
+                  )}
                 </span>
               ))
             : null}
