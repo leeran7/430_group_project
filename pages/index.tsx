b100b5e8229c5464eae7405256f3d2df56caac80
Sebastian Stec <sebastianstec@Sebastians-MacBook-Pro.local>
2022-11-21 15:14:24 -0500

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,8 @@ import { CgSpinner } from "react-icons/cg";
 
 const Home: NextPage = () => {
   const [user] = useUser();
-  const { games, loading, query } = useGetGames();
+  const { games, popularGames, loading, query } = useGetGames();
+
 
   if (loading) {
     return (
@@ -29,33 +30,39 @@ const Home: NextPage = () => {
         <p>Page {query.page ?? 1}</p>
         <Button isNext label="Next Page" />
       </div>
+
+
+
+
+
+      <div>
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h1 className="sr-only">Products</h1>
+        <h1>Popular Products</h1>
         <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {games.length > 0
-            ? games?.map((game) => (
-                <span key={game.id} className="relative flex flex-col">
-                  <Link href={`/game/${game.id}`}>
+          {popularGames.length > 0
+            ? games?.map((popularGames) => (
+                <span key={popularGames.id} className="relative flex flex-col">
+                  <Link href={`/game/${popularGames.id}`}>
                     <a className="group flex flex-col gap-y-1 p-2 hover:shadow-md focus:shadow-md hover:md:shadow-2xl  rounded-lg shadow-gray-300 hover:sm:scale-110 transition-all ease-in-out">
                       <div className="flex flex-col overflow-hidden rounded-lg bg-gray-200">
                         <img
-                          src={game.background_image}
-                          alt={game.name}
+                          src={popularGames.background_image}
+                          alt={popularGames.name}
                           className="h-52 sm:h-40"
                         />
                       </div>
                       <h3 className="text-gray-700 font-semibold">
-                        {game.name}
+                        {popularGames.name}
                       </h3>
                       <p className="text-gray-800">
-                        Price: {getPricing(game.released, game.rating)}
+                        Price: {getPricing(popularGames.released, popularGames.rating)}
                       </p>
                       <p className="text-gray-900">
-                        Release Date: {game.released}
+                        Release Date: {popularGames.released}
                       </p>
-                      <p className="flex gap-x-1">Rating: {game.rating} / 5</p>
+                      <p className="flex gap-x-1">Rating: {popularGames.rating} / 5</p>
                       <span className="flex gap-x-1">
-                        {game.platforms
+                        {popularGames.platforms
                           .map((platform) => platform.platform.name)
                           .map(getSymbols)}
                       </span>
@@ -63,7 +70,7 @@ const Home: NextPage = () => {
                   </Link>
                   {user && (
                     <button
-                      onClick={() => alert(game.id)}
+                      onClick={() => alert(popularGames.id)}
                       className="absolute bottom-0 right-0 py-2 px-5 z-10 bg-green-400 hover:bg-green-500 rounded"
                     >
                       Add to cart
@@ -74,6 +81,21 @@ const Home: NextPage = () => {
             : null}
         </div>
       </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       <div className="flex gap-x-10 items-center justify-center text-center py-10">
         <Button label="Previous" />
@@ -86,6 +108,8 @@ const Home: NextPage = () => {
 
 const useGetGames = () => {
   const [games, setGames] = useState<Props["games"]>([]);
+  const [popularGames, setPopularGames] = useState<Props["games"]>([]);
+
   const [loading, setLoading] = useState(true);
 
   const { query } = useRouter();
@@ -96,13 +120,17 @@ const useGetGames = () => {
     const fetchData = async () => {
       const rawgApiClient = new RawgApiClient();
       let games: Game[] = [];
+      let popularGames: Game[] = [];
+      let recentGames: Game[] = [];
 
       if (query.search) {
         const search = query.search.toString();
         games = await rawgApiClient.searchGames(search, page);
       } else {
         //games = await rawgApiClient.getGames(page);
-        games = await rawgApiClient.getPopularGames(page);
+        games = await rawgApiClient.getGames(page);
+        popularGames = await rawgApiClient.getPopularGames(page);
+        recentGames = await rawgApiClient.getRecentGames(page);
       }
 
       const mappedGames =
@@ -116,14 +144,31 @@ const useGetGames = () => {
             "background_image",
           ])
         ) ?? [];
+
+
+        const mappedPopGames =
+        popularGames.map((game) =>
+          pick(game, [
+            "id",
+            "name",
+            "platforms",
+            "rating",
+            "released",
+            "background_image",
+          ])
+        ) ?? [];
+
+
+
       setGames(mappedGames);
+      setPopularGames(mappedPopGames)
       setLoading(false);
     };
 
     fetchData();
   }, [query.page, query.search, page]);
 
-  return { games, loading, query };
+  return { games, popularGames, loading, query };
 };
 
 export type Props = {
