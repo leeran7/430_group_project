8b0708a10335c8c9babfbcc2dc30974f44daae57
Leeran <leeran7@gmail.com>
2022-11-30 15:49:02 -0500

    fixed jest bug

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,19 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import React from "react";
 export * from "@testing-library/react";
 import Index from "../pages/index";
 import { createMockRouter } from "../test-utils/createMockRouter";
 import { RouterContext } from "next/dist/shared/lib/router-context";
-
-// describe("Index Page", () => {
-//   it("should find name", () => {
-//     render(<Index />);
-
-//     const GameCard = screen.getByRole("GameCard");
-
-//     expect(GameCard).toBeInTheDocument();
-//   });
-// });
 
 it("renders div", () => {
   render(
--- jest.config.ts
+++ jest.config.ts
@@ -1,25 +1,23 @@
-const nextJest = require('next/jest')
+import nextJest from "next/jest";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
+    "^@/components/(.*)$": "<rootDir>/components/$1",
 
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
   },
-  testEnvironment: 'jest-environment-jsdom',
-  transformIgnorePatterns: ['^.+\\.js$'],
+  testEnvironment: "jest-environment-jsdom",
+  transformIgnorePatterns: ["^.+\\.js$"],
   resolver: "<rootDir>/jest.resolver.js", // or <rootDir>/path/to/your/resolver.js
-
-}
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
