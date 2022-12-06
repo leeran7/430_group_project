e992529755ff041c6962f0f70375e253fa247d34
Gary Feng <66232937+TechGF@users.noreply.github.com>
2022-12-06 18:00:33 -0500

    restyled detail page and add expand toggle to description (#19)
    * restyled detail page
    
    * added trailer to detial page
    
    Co-authored-by: Leeran <leeran7@gmail.com>

--- pages/game/[slug].tsx
+++ pages/game/[slug].tsx
@@ -12,8 +12,9 @@ import { useUser } from "../../components/firebase";
 import { useUpdateUser } from "../../hooks/useUpdateUser";
 import { pick } from "lodash";
 import clsx from "clsx";
+import React, { useState } from "react";
 
-const Home: NextPage<Props> = ({ game }) => {
+const Home: NextPage<Props> = ({ game, trailer }) => {
   const [user] = useUser();
   const {
     loading,
@@ -25,6 +26,7 @@ const Home: NextPage<Props> = ({ game }) => {
     getIsInCart,
   } = useUpdateUser();
   const { isFallback } = useRouter();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   if (isFallback || !game || loading) {
     return (
@@ -33,126 +35,202 @@ const Home: NextPage<Props> = ({ game }) => {
       </div>
     );
   }
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   const isInWishList = getIsInWishList(game.id);
   const isOwned = getIsOwned(game.id);
   const isInCart = getIsInCart(game.id);
   const price = getPricing(game.released, game.rating);
+  const imagesAndVideos = [
+    { type: "image", url: game.background_image },
+    { type: "image", url: game.background_image_additional },
+    trailer ? { type: "video", url: trailer } : {},
+  ].filter((i) => !!i.url);
   return (
-    <div className="relative">
-      <Carousel
-        autoPlay
-        className="object-cover mt-3 ml-auto mr-auto h-1/2 w-5/6"
-      >
-        {[game.background_image, game.background_image_additional].map((i) => (
-          <div key={i}>
-            <img className="rounded-md" src={i} alt={game.name} />
-            <p className="legend">{game.name}</p>
-          </div>
-        ))}
-      </Carousel>
-
-      <div className="grid grid-cols-3 absolute gap-x-5 mx-20">
-        <div className="col-span-3 px-20 py-10 bg-white z-10">
-          <div className="flex flex-col gap-y-1">
-            <h1 className="text-6xl font-semibold">
-              <a href={game.website} className="hover:text-indigo-600">
-                {game.name}
-              </a>
-            </h1>
-            <p className="tracking-wider">{game.description_raw}</p>
-            <p>
-              Price: <span className="semi-bold">{price}</span>
-            </p>
-            <p>
-              Release Date: <span className="semi-bold">{game.released}</span>
-            </p>
-            {game.reddit_url && (
-              <a
-                href={game.reddit_url}
-                className="text-2xl hover:text-indigo-600"
-              >
-                <FaReddit />
-              </a>
-            )}
-            {game.rating && <p>Rating: {game.rating}</p>}
-            <div className="flex gap-x-3">
-              {game.ratings.map((r) => (
-                <div key={r.id} className="capitalize">
-                  {r.title} - {r.count.toLocaleString()} - {r.percent}%,{" "}
+    <div className="relative px-20 py-10">
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2">
+          <Carousel
+            renderThumbs={() =>
+              imagesAndVideos.map((i) => (
+                <div key={i.url}>
+                  {i.type === "image" ? (
+                    <img className="rounded-md" src={i.url} alt={game.name} />
+                  ) : trailer ? (
+                    <video className="rounded-md" muted autoPlay src={i.url} />
+                  ) : null}
                 </div>
-              ))}
-            </div>
-            {game.esrb_rating?.name ? (
-              <p>ESRB Rating: {game.esrb_rating.name}</p>
-            ) : null}
-            <span className="flex gap-x-3">
-              {game.platforms
-                .map((platform) => platform.platform.name)
-                .map(getSymbols)}
-            </span>
-          </div>
-          {user && !isOwned && (
-            <div className="flex justify-end">
-              <button
-                onClick={async () => {
-                  if (isInWishList) {
-                    await onWishlistDelete(game.id);
-                  } else {
-                    await onWishlistAdd({
-                      ...game,
-                      price,
-                    });
-                  }
-                }}
-                className={clsx(
-                  "mr-4 py-2 px-5 self-end text-center flex flex-col items-center justify-end z-10 rounded fill-red-600 border-2 border-transparent hover:border-red-600",
-                  isInWishList && "border-red-600 hover:bg-red-100"
+              ))
+            }
+            autoPlay
+            className="object-cover mt-3 ml-auto mr-auto"
+          >
+            {imagesAndVideos.map((i) => (
+              <div key={i.url}>
+                {i.type === "image" ? (
+                  <img className="rounded-md" src={i.url} alt={game.name} />
+                ) : (
+                  trailer && (
+                    <video
+                      className="rounded-md"
+                      controls
+                      muted
+                      autoPlay
+                      src={i.url}
+                    />
+                  )
                 )}
-                type="button"
-              >
-                <svg
-                  className="w-6 h-6 "
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-              </button>
-              <button
-                onClick={async () => {
-                  await onAddCartItem({
-                    ...pick(game, [
-                      "id",
-                      "name",
-                      "background_image",
-                      "rating",
-                      "released",
-                      "slug",
-                    ]),
-                    price,
-                  });
-                }}
-                className={clsx(
-                  isInCart
-                    ? "bg-gray-600 cursor-not-allowed"
-                    : "bg-green-600 hover:bg-green-500",
-                  "py-2 px-5 z-10 rounded"
-                )}
-              >
-                Add to Cart
-              </button>
-            </div>
-          )}
+              </div>
+            ))}
+          </Carousel>
         </div>
 
-        <CarouselWithLabel type={game.developers} label="Developers" />
-        <CarouselWithLabel type={game.tags} label="Tags" />
-        <CarouselWithLabel type={game.genres} label="Genres" />
+        <div className="w-full md:w-1/2">
+          <div className="col-span-3 px-20 bg-white z-10">
+            <div className="flex flex-col gap-y-2">
+              <h1 className="text-6xl font-semibold">
+                {game.website ? (
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={game.website}
+                    className="hover:text-indigo-600"
+                  >
+                    {game.name}
+                  </a>
+                ) : (
+                  game.name
+                )}
+              </h1>
+              <div className="relative">
+                <p
+                  className={clsx(
+                    "text-black-700 text-sm md:text-base overflow-hidden text-ellipsis",
+                    isExpanded ? "h-auto" : "h-24"
+                  )}
+                >
+                  {game.description_raw}
+                </p>
+                {!isExpanded && (
+                  <p className="absolute bottom-0 -right-2">...</p>
+                )}
+              </div>
+              <button
+                className="hover:text-indigo-600"
+                onClick={toggleDescription}
+              >
+                show {isExpanded ? "less" : "more"}
+              </button>
+              <h2 className="flex items-center gap-x-3 text-3xl font-semibold">
+                {price}{" "}
+                {game.reddit_url && (
+                  <a
+                    href={game.reddit_url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-2xl hover:text-indigo-600"
+                  >
+                    <FaReddit />
+                  </a>
+                )}
+              </h2>
+              <p>Released: {game.released}</p>
+
+              {game.rating && <p>Rating: {game.rating}</p>}
+              <div className="flex gap-x-10">
+                {game.ratings.map((r) => (
+                  <div
+                    key={r.id}
+                    className="capitalize flex flex-col items-center justify-between"
+                  >
+                    <p>{r.title}</p> <p>{r.count.toLocaleString()}</p>{" "}
+                    <p>{r.percent}%</p>
+                  </div>
+                ))}
+              </div>
+              {game.esrb_rating?.name ? (
+                <p>ESRB Rating: {game.esrb_rating.name}</p>
+              ) : null}
+              <span className="flex gap-x-3">
+                {game.platforms
+                  .map((platform) => platform.platform.name)
+                  .map(getSymbols)}
+              </span>
+            </div>
+            {user && !isOwned && (
+              <div className="flex justify-end">
+                <button
+                  onClick={async () => {
+                    if (isInWishList) {
+                      await onWishlistDelete(game.id);
+                    } else {
+                      await onWishlistAdd({
+                        ...game,
+                        price,
+                      });
+                    }
+                  }}
+                  className={clsx(
+                    "mr-4 py-2 px-5 self-end text-center flex flex-col items-center justify-end z-10 rounded fill-red-600 border-2 border-transparent hover:border-red-600",
+                    isInWishList && "border-red-600 hover:bg-red-100"
+                  )}
+                  type="button"
+                >
+                  <svg
+                    className="w-6 h-6 "
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                </button>
+                <button
+                  onClick={async () => {
+                    await onAddCartItem({
+                      ...pick(game, [
+                        "id",
+                        "name",
+                        "background_image",
+                        "rating",
+                        "released",
+                        "slug",
+                      ]),
+                      price,
+                    });
+                  }}
+                  className={clsx(
+                    isInCart
+                      ? "bg-gray-600 cursor-not-allowed"
+                      : "bg-green-600 hover:bg-green-500",
+                    "py-2 px-5 z-10 rounded"
+                  )}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between space-x-5 mt-10">
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
+          <CarouselWithLabel type={game.developers} label="Developers" />
+        </div>
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
+          <CarouselWithLabel type={game.tags} label="Tags" />
+        </div>
+        <div className="w-full md:w-1/3">
+          <CarouselWithLabel type={game.genres} label="Genres" />
+        </div>
       </div>
     </div>
   );
@@ -160,6 +238,7 @@ const Home: NextPage<Props> = ({ game }) => {
 
 export type Props = {
   game: Game;
+  trailer: string | undefined | null;
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -179,9 +258,9 @@ export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
       notFound: true,
     };
   }
-
+  const trailer = await rawgApiClient.getTrailer(game.slug);
   return {
-    props: { game },
+    props: { game, trailer: trailer.results[0]?.data?.max ?? null },
   };
 };
 
