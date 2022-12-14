952efc96f670615243fed3eab4412599ba1b76c7
Leeran <leeran7@gmail.com>
2022-12-14 13:36:06 -0500

    fix marginsof game container

--- pages/index.tsx
+++ pages/index.tsx
@@ -238,11 +238,11 @@ const PageButtons: React.FC<{ pageOneOrNoQuery: boolean }> = ({
 export const GameContainer: React.FC<
   PropsWithChildren<{ label: string; className?: string }>
 > = ({ children, label, className }) => (
-  <div className={clsx("p-4 sm:pb-16 sm:px-6 w-full lg:px-32", className)}>
+  <div className={clsx("p-4 sm:pb-16 sm:px-6 w-full lg:px-20", className)}>
     <h1 className="text-2xl font-semibold tracking-wider text-gray-700 mb-3">
       {label}
     </h1>
-    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 xl:gap-x-8">
+    <div className="grid grid-cols-1 gap-14 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 xl:gap-x-8">
       {children}
     </div>
   </div>
